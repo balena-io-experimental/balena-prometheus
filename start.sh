2d33f5f0520591b5485965312cc8822857c1a8e7
gaudi <craig@resin.io>
2016-08-04 15:06:07 +0100

    change port

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 # Start the node exporter
 cd /etc/node_exporter-$NODE_EXPORTER_VERSION.$DIST_ARCH \
-  && ./node_exporter -web.listen-address ":80" \
-    -storage.local.path "/data" -storage.local.retention 360h0m0s
+  && ./node_exporter -web.listen-address ":80"
